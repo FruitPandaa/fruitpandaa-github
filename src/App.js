b8841733f7f2469efcb987d423f65d4e41cb282b
FruitPandaa <fruitpandaa@gmail.com>
2019-12-03 16:37:49 +0100

    added prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+/////////////////////////////////////////////////////////////////// yarn
+//
+// yarn start
+// Starts the development server.
+//
+// yarn build
+// Bundles the app into static files for production.
+//
+// yarn test
+// Starts the test runner.
+//
+// yarn eject
+// Removes this tool and copies build dependencies, configuration files
+// and scripts into the app directory. If you do this, you can’t go back!
+//
+/////////////////////////////////////////////////////////////////// git
+//
+// Create a directory to contain the project.
+// Go into the new directory.
+// Type git init.
+// Write some code.
+// Type git add . to add the files (see the typical use page).
+// Type git commit.
+//
+/////////////////////////////////////////////////////////////////// npx
+//
+// Creating react app
+// npx create-react-app app-name
 
 function App() {
   return (
@@ -20,7 +49,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
